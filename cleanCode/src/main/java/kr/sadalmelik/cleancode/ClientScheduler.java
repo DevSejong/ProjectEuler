e306e3ce2cfef1a05d66780cc02f2f3d0da3a831
SejongPark <devsejong@gmail.com>
2014-11-18 10:18:18 +0900

    스터디용 args프로젝트 생성

--- cleanCode/src/main/java/kr/sadalmelik/cleancode/ClientScheduler.java
+++
@@ -1,8 +0,0 @@
-package kr.sadalmelik.cleancode;
-
-/**
- * Created by SejongPark on 14. 10. 26..
- */
-public interface ClientScheduler {
-    void schedule(ClientRequestProcessor requestProcessor);
-}
